d65673b00535d40812d4afbd21e2794e5a8f3563
manorajesh <mano@raje.sh>
2022-08-04 16:11:14 +0530

    Dealt with printing unicode canonization and added basic command ui

--- hexdump.py
+++ hexdump.py
@@ -2,25 +2,16 @@ import os
 from ctypes import *
 import ctypes.util
 from binascii import unhexlify
+import time
 
-def print_hex(data):
-    for i in range(0, len(data), 16):
-        print("{:08x} ".format(i), end="")
-        print(" ".join(data[i:i+8]), end="  ")
-        print(" ".join(data[i+8:i+16]), end="  ")
-        #print(bytes.fromhex("".join(data[i:i+16])).decode("ascii", "ignore"))
-        print(unhexlify("".join(data[i:i+16])).decode("utf-8", "replace"))
-
-def cprint_hex(data):
+def cprint_hex_w_unicode(data):
     libc = CDLL(ctypes.util.find_library("c"))
     for i in range(0, len(data), 16):
         libc.printf(b"%08x ", i)
-        libc.printf(b"%s  %s  |%s|\n", bytes(" ".join(data[i:i+8]), "ascii"), bytes(" ".join(data[i+8:i+16]), "ascii"), bytes(repr(unhexlify("".join(data[i:i+16])).decode("utf-8", "replace")), "utf-8"))
+        libc.printf(b"%s  %s |%s|\n", bytes(" ".join(data[i:i+8]), "ascii"), bytes(" ".join(data[i+8:i+16]), "ascii"), bytes(repr(unhexlify("".join(data[i:i+16])).decode("utf-8", "replace")), "utf-8"))
 
-path = ("bintest.bin")
-path = os.path.expanduser(path)
-
-with open(path, "rb") as f:
-    raw = f.read().hex()
-    split = [raw[i:i+2] for i in range(0, len(raw), 2)]
-    cprint_hex(split)
+def cprint_hex_wout_unicode(data):
+    libc = CDLL(ctypes.util.find_library("c"))
+    for i in range(0, len(data), 16):
+        libc.printf(b"%08x ", i)
+        libc.printf(b"%s  %s\n", bytes(" ".join(data[i:i+8]), "ascii"), bytes(" ".join(data[i+8:i+16]), "ascii"))
